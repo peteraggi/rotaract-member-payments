b44abbfc916063cb0d0c9dd4cf54e3ad4ea6456e
peteraggi <aggipeter25@gmail.com>
2025-07-02 12:55:22 +0300

    changes

--- middleware.ts
+++ middleware.ts
@@ -28,9 +28,10 @@ export async function middleware(request: NextRequest) {
   // if (isAuthRoute && token) {
   //   return NextResponse.redirect(new URL('/registration', request.url));
   // }
-  if (isAuthRoute && token) {
+  if (isAuthRoute && token && !request.nextUrl.searchParams.has('callbackUrl')) {
     return NextResponse.redirect(new URL('/registration', request.url));
   }
+  
 
   return NextResponse.next();
 }
