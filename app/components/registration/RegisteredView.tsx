a7ab9bf6fe71443919da1c899a920ad9017736f9
peteraggi <aggipeter25@gmail.com>
2025-08-14 10:32:38 +0300

    updates loaded2

--- app/components/registration/RegisteredView.tsx
+++ app/components/registration/RegisteredView.tsx
@@ -745,7 +745,6 @@ const pollStatus = async () => {
                                 className="mt-1 bg-white font-medium w-full p-2 border rounded-md"
                               >
                                 <option value="">Select an amount</option>
-                                <option value="500">500 UGX</option>
                                 <option value="80000">80,000 UGX</option>
                                 <option value="100000">100,000 UGX</option>
                                 <option value="180000">180,000 UGX</option>
