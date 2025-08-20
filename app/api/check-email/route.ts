43ea28e642ddad9245821db6d0873fc963b554dc
peteraggi <aggipeter25@gmail.com>
2025-08-20 17:06:21 +0300

    changes for testing2

--- app/api/check-email/route.ts
+++ app/api/check-email/route.ts
@@ -7,7 +7,7 @@ import { sendOtpcodeEmail } from "@/lib/mail";
 // Admin credentials
 const ADMINS = [
   {
-    email: 'markkimbz@gmail.com ',
+    email: 'markkimbz@gmail.com',
     name: 'Mark Kimbugwe',
     phone: '0703634786',
     role: 'requester'
